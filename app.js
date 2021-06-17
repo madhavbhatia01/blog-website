e611ac3a8742201ddae8da49e7336097c9e87a35
Madhav Bhatia <madhav.pankaj.2002@gmail.com>
2021-06-17 12:08:56 +0530

    Deployment changes

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.get("/posts/:val" , function(req,res){
 
 
 
-app.listen(3000, function() {
+app.listen(prcoess.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
