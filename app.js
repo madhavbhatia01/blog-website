f047c379883dbe296b11e9a20a3c6d4a1f043d56
Madhav Bhatia <madhav.pankaj.2002@gmail.com>
2022-01-25 04:13:40 +0530

    Add procfile and images to the site

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const _ = require("lodash");
 
 let arr = [];
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Are you a blogger ? Well you have come to the right place. Start writing your blog by clicking the COMPOSE button above. ";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -23,7 +23,8 @@ mongoose.connect(process.env.SECRET, {useNewUrlParser: true, useUnifiedTopology:
 
 const postSchema = {
    heading: String,
-   content: String
+   content: String,
+    imag: String
 };
 
 const Post = mongoose.model("Post", postSchema);
@@ -53,7 +54,8 @@ app.get("/compose" , function(req,res){
 app.post("/compose" , function(req,res){
   const post = new Post ({
      heading: req.body.Heading,
-     content: req.body.Body
+     content: req.body.Body,
+    imag : req.body.Image
    });
    post.save(function(err){
      if (!err){
@@ -78,7 +80,7 @@ app.get("/posts/:val" , function(req,res){
 
   Post.findOne({_id: req.params.val}, function(err, foundItem){
     if(!err){
-      res.render("post" , {heading: foundItem.heading , text : foundItem.content});
+      res.render("post" , {heading: foundItem.heading , text : foundItem.content , imag : foundItem.imag});
     }
   })
 
